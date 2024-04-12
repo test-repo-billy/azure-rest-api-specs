d1c79cdaf1181dbaf83bca70eacf32d2e815d531
Zhenhua Hu <zhhu@microsoft.com>
2024-04-12 16:29:03 +0800

    test

--- specification/purview/resource-manager/Microsoft.Purview/stable/2021-12-01/terraform/Microsoft.Purview/main.tf
+++ specification/purview/resource-manager/Microsoft.Purview/stable/2021-12-01/terraform/Microsoft.Purview/main.tf
@@ -12,7 +12,7 @@ provider "azapi" {
 
 variable "resource_name" {
   type    = string
-  default = "acctest8566"
+  default = "acctest0001"
 }
 
 variable "location" {
@@ -20,62 +20,130 @@ variable "location" {
   default = "westeurope"
 }
 
-// OperationId: DefaultAccounts_Get
-// GET /providers/Microsoft.Purview/getDefaultAccount
-data "azapi_resource_action" "getDefaultAccount" {
-  type        = "Microsoft.Purview@2021-12-01"
-  resource_id = "/providers/Microsoft.Purview"
-  action      = "getDefaultAccount"
-  method      = "GET"
+resource "azapi_resource" "resourceGroup" {
+  type     = "Microsoft.Resources/resourceGroups@2020-06-01"
+  name     = var.resource_name
+  location = var.location
 }
 
-// OperationId: DefaultAccounts_Remove
-// POST /providers/Microsoft.Purview/removeDefaultAccount
-resource "azapi_resource_action" "removeDefaultAccount" {
-  type        = "Microsoft.Purview@2021-12-01"
-  resource_id = "/providers/Microsoft.Purview"
-  action      = "removeDefaultAccount"
-  method      = "POST"
-}
-
-// OperationId: DefaultAccounts_Set
-// POST /providers/Microsoft.Purview/setDefaultAccount
-resource "azapi_resource_action" "setDefaultAccount" {
-  type        = "Microsoft.Purview@2021-12-01"
-  resource_id = "/providers/Microsoft.Purview"
-  action      = "setDefaultAccount"
-  method      = "POST"
+resource "azapi_resource" "virtualNetwork" {
+  type      = "Microsoft.Network/virtualNetworks@2022-07-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
   body = jsonencode({
-    accountName       = "myDefaultAccount"
-    resourceGroupName = "rg-1"
-    scope             = "12345678-1234-1234-1234-12345678abcd"
-    scopeTenantId     = "12345678-1234-1234-1234-12345678abcd"
-    scopeType         = "Tenant"
-    subscriptionId    = "12345678-1234-1234-1234-12345678aaaa"
+    properties = {
+      addressSpace = {
+        addressPrefixes = [
+          "10.0.0.0/16",
+        ]
+      }
+      dhcpOptions = {
+        dnsServers = [
+        ]
+      }
+      subnets = [
+      ]
+    }
   })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+  ignore_body_changes       = ["properties.subnets"]
 }
 
-data "azapi_resource" "subscription" {
-  type                   = "Microsoft.Resources/subscriptions@2020-06-01"
-  response_export_values = ["*"]
-}
-
-data "azapi_resource_id" "subscriptionScopeProvider" {
-  type      = "Microsoft.Resources/providers@2020-06-01"
-  parent_id = data.azapi_resource.subscription.id
-  name      = "Microsoft.Purview"
-}
-
-// OperationId: Accounts_CheckNameAvailability
-// POST /subscriptions/{subscriptionId}/providers/Microsoft.Purview/checkNameAvailability
-resource "azapi_resource_action" "checkNameAvailability" {
-  type        = "Microsoft.Purview@2021-12-01"
-  resource_id = data.azapi_resource_id.subscriptionScopeProvider.id
-  action      = "checkNameAvailability"
-  method      = "POST"
+resource "azapi_resource" "subnet" {
+  type      = "Microsoft.Network/virtualNetworks/subnets@2022-07-01"
+  parent_id = azapi_resource.virtualNetwork.id
+  name      = var.resource_name
   body = jsonencode({
-    name = "account1"
-    type = "Microsoft.Purview/accounts"
+    properties = {
+      addressPrefix = "10.0.2.0/24"
+      delegations = [
+      ]
+      privateEndpointNetworkPolicies    = "Enabled"
+      privateLinkServiceNetworkPolicies = "Enabled"
+      serviceEndpointPolicies = [
+      ]
+      serviceEndpoints = [
+      ]
+    }
   })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
 }
 
+resource "azapi_resource" "networkInterface" {
+  type      = "Microsoft.Network/networkInterfaces@2022-07-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = jsonencode({
+    properties = {
+      enableAcceleratedNetworking = false
+      enableIPForwarding          = false
+      ipConfigurations = [
+        {
+          name = "testconfiguration1"
+          properties = {
+            primary                   = true
+            privateIPAddressVersion   = "IPv4"
+            privateIPAllocationMethod = "Dynamic"
+            subnet = {
+              id = azapi_resource.subnet.id
+            }
+          }
+        },
+      ]
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
+
+resource "azapi_resource" "virtualMachine" {
+  type      = "Microsoft.Compute/virtualMachines@2023-03-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = jsonencode({
+    properties = {
+      hardwareProfile = {
+        vmSize = "Standard_F2"
+      }
+      networkProfile = {
+        networkInterfaces = [
+          {
+            id = azapi_resource.networkInterface.id
+            properties = {
+              primary = false
+            }
+          },
+        ]
+      }
+      osProfile = {
+        adminPassword = "Password1234!"
+        adminUsername = "testadmin"
+        computerName  = "hostname230630032848831819"
+        linuxConfiguration = {
+          disablePasswordAuthentication = false
+        }
+      }
+      storageProfile = {
+        imageReference = {
+          offer     = "UbuntuServer"
+          publisher = "Canonical"
+          sku       = "16.04-LTS"
+          version   = "latest"
+        }
+        osDisk = {
+          caching                 = "ReadWrite"
+          createOption            = "FromImage"
+          name                    = "myosdisk1"
+          writeAcceleratorEnabled = false
+        }
+      }
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
