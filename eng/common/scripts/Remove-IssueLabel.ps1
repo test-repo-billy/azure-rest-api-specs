3859e7c404e6583ffcb2c96d70c28b20a1a84d12
ray chen <raychen@microsoft.com>
2025-01-15 22:42:08 +0000

    Added label removal feature

---
+++ eng/common/scripts/Remove-IssueLabel.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$RepoOwner,
+
+  [Parameter(Mandatory = $true)]
+  [string]$RepoName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$IssueNumber,
+
+  [Parameter(Mandatory = $true)]
+  [string]$LabelName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$AuthToken
+)
+
+. (Join-Path $PSScriptRoot common.ps1)
+
+try {
+  Remove-GitHubIssueLabel -RepoOwner $RepoOwner -RepoName $RepoName `
+  -IssueNumber $IssueNumber -LabelName $LabelName -AuthToken $AuthToken
+}
+catch {
+  LogError "Remove-GithubIssueLabels failed with exception:`n$_"
+  exit 1
+}
