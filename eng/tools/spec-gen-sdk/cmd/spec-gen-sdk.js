f4679f7d8caa7f43fe47b23f2bf6ce9bd1bd0354
ray chen <raychen@microsoft.com>
2025-02-20 00:26:50 +0000

    fix npm errros

--- eng/tools/spec-gen-sdk/cmd/spec-gen-sdk.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import("../dist/cli/cli.js")
-  .then(() => {
-    console.log('CLI module loaded successfully.');
-  })
-  .catch(err => {
-    console.error('Error loading CLI module:', err);
-  });
