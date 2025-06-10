e804b25d15b85b107fa30221b76220d2dcfecd68
Mike Harder <mharder@microsoft.com>
2025-06-10 17:30:23 +0000

    Revert "[LintDiff] Skip readme.md files with no "input-file:" string (#35174)"
    This reverts commit fc12f4e13c3ce328372237147d4d7438ed980c72.

--- eng/tools/lint-diff/test/processChanges.test.ts
+++ eng/tools/lint-diff/test/processChanges.test.ts
@@ -249,13 +249,4 @@ describe("buildState", () => {
       ),
     ).not.toThrow();
   });
-
-  test.skipIf(isWindows())("does not include readme files that has no input-file:", async () => {
-    const actual = await buildState(
-      ["specification/no-input-file/readme.md"],
-      "test/fixtures/buildState/",
-    );
-
-    expect(actual).toEqual([new Map<string, ReadmeAffectedTags>(), []]);
-  });
 });
